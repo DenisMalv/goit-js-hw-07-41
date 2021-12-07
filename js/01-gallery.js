4d5f3455da1d0c1a100e692eb02d9c7840b28090
DenisMalv <kissed03@gmail.com>
2021-12-07 15:42:09 +0200

    complete tz

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,65 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const refs = {
+    gallery: document.querySelector('.gallery'),
+    LEFT_ARROWKEY : 'ArrowLeft',
+    RIGHT_ARROWKEY:'ArrowRight',
+    ESC_KEY :'Escape',
+}
+const {gallery,LEFT_ARROWKEY,RIGHT_ARROWKEY,ESC_KEY} = refs
+console.log(gallery)
+
+gallery.insertAdjacentHTML('beforeend', createGaleryMarkup())
+
+function createGaleryMarkup() {
+    return  galleryItems.map(({ preview,original, description }) => `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`).join('')
+    
+}
+
+gallery.addEventListener('click',openModal)
+
+
+function openModal(event) {
+    event.preventDefault();
+
+    window.addEventListener('keydown', closeModal)
+    
+	const modalWindowMarkup = basicLightbox.create(`<img src="${event.target.dataset.source}">`, {
+        onShow: () => {
+                console.log('onShow', event)                      
+        },
+        onClose: (event) => {
+            window.removeEventListener('keydown', closeModal)
+            console.log('onClose', event.visible())
+        }
+    })
+
+    showModal()
+    closeModal(event)
+
+    function showModal() {
+        modalWindowMarkup.show()
+    }
+    function closeModal(event) {
+        if (event.code === ESC_KEY) {
+            modalWindowMarkup.close()
+        }
+    }
+}
+
+   
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery')
+console.log(gallery)
+
+gallery.insertAdjacentHTML('beforeend', createGaleryMarkup())
+
+function createGaleryMarkup() {
+    return galleryItems.map(({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`).join('')   
+}
+
+const lightBox = new SimpleLightbox('.gallery a', {
+    captions:true,captionSelector:'img',captionType:'attr',captionsData:`alt`,captionPosition:'bottom',captionDelay:250
+});
